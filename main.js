c23f63123846f45870a139bcd4fba0e47a4d3215
aaron wurie <aaronw0418@gmail.com>
2019-04-11 12:00:57 -0400

    finished up

--- main.js
+++ main.js
@@ -1,53 +1,71 @@
 var input = document.querySelector("#input");
 
-var unit = document.querySelector("#unit")
+var unit = document.querySelector("#unit");
 
 input.addEventListener("input", convert);
 
-unit.addEventListener("input", convert)
+unit.addEventListener("input", convert);
+
 
 function convert() {
     var value = input.value;
     var unitValue = unit.value;
-
     var output = document.querySelector("#output");
+
     if(value !=="") {
-    output.style.display = "block"
+    output.style.display = "block";
     } else {
-        output.style.display = "none"
+        output.style.display = "none";
+    }
 
 
         switch(unitValue) {
             case "lbs":
-
+                convertLbs();
+                break;
             case "grams":
-
+                convertGrams(value)
+                break;
             case "kilograms":
-
+                break;
             case "ounces":
-
+                break;
             case "meters":
-
-            case "":
+                break;
+            case "miles":
+                break;
+            case "inches":
+                break;
         }
-    // get value of user input
-    var value = input.value;
-    }
+    
+}
+
+    var card1 = document.querySelector("#card-1");
+    var card2 = document.querySelector("#card-2");
+    var card3 = document.querySelector("#card-3");
+
+    
     //grams
-    function convertLbs(){
+    function convertLbs(value) {
         
-    card1.querySelector('h4').textContent ="Grams:"
+    card1.querySelector('h4').textContent="GRAMS:"
+    card2.querySelector('h4').textContent="KILOGRAMS:"
+    card3.querySelector('h4').textContent="OUNCES:"
 
-    card2.querySelector('h4').textContent ="Kilograms:"
-
-    card3.querySelector('h4').textContent ="ounces:"
-
-
-    card1.querySelector('div').textContent = value * 453.592
-
-    card2.querySelector('div').textContent = value * 453.592/1000
-
+    card1.querySelector('div').textContent = value * 453.592;
+    card2.querySelector('div').textContent = value * 453.592 / 1000;
     card3.querySelector('div').textContent = value * 16
+    }
+
+    function convertGrams(value) {
+    
+     card1.querySelector('h4').textContent="KILOGRAMS:"
+     card2.querySelector('h4').textContent="OUNCES:"
+     card3.querySelector('h4').textContent="LBS:"
+    
+     card1.querySelector('div').textContent = value * 1000;
+     card2.querySelector('div').textContent = value * 0.035274;
+      card3.querySelector('div').textContent = value * 0.00220462
 
 
     }
